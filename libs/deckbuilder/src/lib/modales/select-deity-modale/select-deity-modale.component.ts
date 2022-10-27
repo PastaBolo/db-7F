2aa4d838dc6ae1fd7ee4bca57fbd0cd71e9b3767
Alain Paolini <alain.paolini291@gmail.com>
2022-10-27 10:56:29 +0200

    refacto: utilisation du CardsService dans la modale de selection de divinité

--- libs/deckbuilder/src/lib/modales/select-deity-modale/select-deity-modale.component.ts
+++ libs/deckbuilder/src/lib/modales/select-deity-modale/select-deity-modale.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { CardsService } from '../../services';
 
 @Component({
   selector: 'seven-fallen-select-deity-modale',
@@ -8,13 +9,14 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrls: ['./select-deity-modale.component.scss'],
 })
 export class SelectDeityModaleComponent {
-  public readonly deities$ = this.http.get<any[]>(
-    `cards/search?type=1&kingdomId=${this.data.kingdomId}`
-  );
+  public readonly deities$ = this.cardsService.search({
+    type: 1,
+    kingdomId: this.data.kingdomId,
+  });
 
   constructor(
-    private readonly http: HttpClient,
     private readonly dialogRef: MatDialogRef<void>,
+    private readonly cardsService: CardsService,
     @Inject(MAT_DIALOG_DATA) public readonly data: { kingdomId: string }
   ) {}
 
